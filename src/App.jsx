38056a85aa62f6d5f663be92d38722669e89b870
abdelrahma <abdelrhman.zero.2007@gmail.com>
2026-01-01 14:34:56 +0200

    Add btn Shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ShopPages from "./pages/ShopPages";
 import ProductDetials from "./pages/ProductDetials";
-
+import { Link } from "react-router-dom";
 export default function App() {
   return (
-    <div className="h-dvh w-full overflow-auto "> 
+    <div className="h-dvh w-full overflow-auto ">
       <BrowserRouter>
         <Routes>
           <Route path="/shop" element={<ShopPages />}></Route>
-          <Route path="/shop/product/:productId" element={<ProductDetials />}></Route>
-        </Routes>
-      </BrowserRouter >
+          <Route
+            path="/shop/product/:productId"
+            element={<ProductDetials />}
+          ></Route>
+        </Routes>{" "}
+        <Link to="/shop" className="btn btn-primary">
+          Shop
+        </Link>
+      </BrowserRouter>{" "}
     </div>
   );
 }
